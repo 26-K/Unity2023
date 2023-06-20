10c5ff94856b9ca6b5d4bb61b738e1f6b1679511
26-k <26nilk@gmail.com>
2023-06-21 04:39:06 +0900

    ワンチャンカードゲーム

---
+++ Unity2023/Assets/BattleCardBase.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BattleCardBase : MonoBehaviour
+{
+    public bool isPush;
+    public bool isSelect;
+    [SerializeField] RectTransform sizePos;
+    float targetPos = 10.0f;
+    float currentPos = 0.0f;
+    private void Update()
+    {
+        currentPos += isSelect ? 0.5f : -0.5f;
+        currentPos = Mathf.Clamp(currentPos, 0.0f, targetPos);
+        Vector2 pos = sizePos.localPosition;
+        pos.y = currentPos;
+        sizePos.localPosition = pos;
+    }
+    public void OnPointerEnter()
+    {
+        isSelect = true;
+    }
+
+
+    public void OnPointerExit()
+    {
+        isSelect = false;
+    }
+
+    public void OnPointerDown()
+    {
+        isPush = true;
+    }
+
+    public void OnPointerUp()
+    {
+        isPush = false;
+    }
+}
